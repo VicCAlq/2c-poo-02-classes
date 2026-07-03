92322dfc1fd9d67dc250ece1e1de3b0fa6a0751f
debora-vitoria-nave <debora.vitoria@nave.org.br>
2026-07-03 11:01:04 -0300

    Update Atv02ClasseJogoCompleto.js

--- src/Atv02ClasseJogoCompleto.js
+++ src/Atv02ClasseJogoCompleto.js
@@ -9,4 +9,35 @@
  *
  * Lembre-se de exportar a classe "Jogo" no final do arquivo usando
  * "export default Jogo"
+ */ 
+/*
+ * [ATV 02 - Jogo Completo]
+ *
+ * Copie a classe da atividade anterior e a modifique com o método abaixo:
+ * jogar: método que recebe um argumento "minutos" (número inteiro). 
+ * Cada 10 minutos jogados aumenta o valor das fasesConcluidas 
+ * em 1 / dificuldade. Quando fasesConcluidas for igual a 
+ * fases, zerado muda de false para true.
+ *
+ * Lembre-se de exportar a classe "Jogo" no final do arquivo usando
+ * "export default Jogo"
  */
+class Jogo {
+      constructor (nomeDoJogo, tipo, qtdJogadores, dificuldade, fases){
+            this.nomeDoJogo = nomeDoJogo;
+            this.tipo = tipo;
+            this.qtdJogadores = qtdJogadores;
+            this.dificuldade = dificuldade;
+            this.fases = fases;
+            this.fasesConcluidas = 0;
+            this.zerado = false;
+}
+      jogar(minutos){
+            this.fasesConcluidas += Math.floor(minutos / 10 / this.dificuldade)
+
+            if (this.fasesConcluidas >= this.fases) {
+                  this.zerado = true
+            }
+      }
+}
+
