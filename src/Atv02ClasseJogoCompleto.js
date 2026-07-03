ed9fd62b56edc7abe8205d568ea65a92b5bb9290
Arthur Emanuel <arthur.emanuel@nave.org.br>
2026-07-03 11:02:12 -0300

    Update Atv02ClasseJogoCompleto.js

--- src/Atv02ClasseJogoCompleto.js
+++ src/Atv02ClasseJogoCompleto.js
@@ -10,3 +10,26 @@
  * Lembre-se de exportar a classe "Jogo" no final do arquivo usando
  * "export default Jogo"
  */
+
+class Jogo {
+    constructor(nomeDoJogo, tipo, qtdJogadores, dificuldade, fases){
+        this.nomeDoJogo = nomeDoJogo;
+        this.tipo = tipo;
+        this.qtdJogadores = qtdJogadores;
+        this.dificuldade = dificuldade; 
+        this.fases = fases;
+        this.fasesConcluidas = 0;
+        this.zerado = false;
+    }
+
+    jogar(minutos) {
+        fasesTerminadas = minutosJogados / 10 / this.dificuldade
+        if (this.minutosJogados>= 10) {
+            this.fasesConcluidas +1 /this.dificuldade
+        } if (this.fasesConcluidas = this.fases) {
+            zerado = true
+        }
+    }
+}
+
+export default Jogo;
