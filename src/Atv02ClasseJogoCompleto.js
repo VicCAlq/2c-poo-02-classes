9509340af8d4999e4dd442e4d5c9b9c9f8b42dbf
Isaac-Andrade-Santos-NAVE <isaac.andrade@nave.org.br>
2026-07-03 14:06:44 +0000

    atv02

--- src/Atv02ClasseJogoCompleto.js
+++ src/Atv02ClasseJogoCompleto.js
@@ -10,3 +10,35 @@
  * Lembre-se de exportar a classe "Jogo" no final do arquivo usando
  * "export default Jogo"
  */
+
+class Jogo {
+    constructor(nome, tipo, qtdJogadores, dificuldade, qtdFases) {
+        this.nomeDoJogo = nome
+        this.tipo = tipo
+        this.qtdJogadores = qtdJogadores
+        this.dificuldade = dificuldade
+        this.fases = qtdFases
+
+        this.fasesConcluidas = 0
+        this.zerado = false
+    }
+
+    jogar(minutos) {
+        this.fasesConcluidas += parseInt(minutos / (this.dificuldade * 10))
+
+        if (this.fasesConcluidas >= this.fases) {
+            this.fasesConcluidas = this.fases
+            this.zerado = true
+        }
+    }
+}
+
+const megaMan = new Jogo("Mega Man", "Plataforma/Atirador", 50000, 3, 10)
+console.table(megaMan)
+megaMan.jogar(40)
+console.log(
+    `Fases concluídas: ${megaMan.fasesConcluidas} \n`,
+    `Zerado? ${megaMan.zerado}`
+)
+
+export default Jogo
