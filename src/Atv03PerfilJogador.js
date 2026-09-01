b35ad0021f4c40ce210e5fdda85b030f61c1bed7
Beatriz Vilaça <beatriz.vilaca@nave.org.br>
2026-09-01 12:00:37 -0300

    Update Atv03PerfilJogador.js

--- src/Atv03PerfilJogador.js
+++ src/Atv03PerfilJogador.js
@@ -12,14 +12,13 @@
  * Lembre-se de exportar a classe "PerfilJogador" no final do arquivo usando
  * "export default PerfilJogador"
  */
+class PerfilJogador {
+  constructor(jogador, email) {
+    this.jogador = jogador;
+    this.email = email;
+    this.jogos = [];
+    this.amigos = [];
+  }
+}
 
-    class PerfilJogador { 
-        constructor(jogador, email){
-            this.jogador = jogador;
-            this.email = email;
-            this.jogos = [];
-            this.amigos = [];
-
-
-        }
-    }
+export default PerfilJogador;
